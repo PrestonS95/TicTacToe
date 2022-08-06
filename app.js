04a925c6d7170cd1940961e07a1bf6de6e7a3cb7
PrestonS95 <prestonsullins95@gmail.com>
2022-08-06 01:37:07 -0500

    trying

--- app.js
+++ app.js
@@ -1,10 +1,5 @@
 let gameState = {
     players: ["x", "o"],
-    // board: [
-    //     ['', '', ''],
-    //     ['', '', ''],
-    //     ['', '', ''],
-    // ],
     board: [
         [null, null, null],
         [null, null, null],
@@ -21,9 +16,7 @@ let gameState = {
 
 let {players, board, turn, currentPlayer } = gameState
 const boardArray = Array.from(document.querySelectorAll('div.cell'))
-boardArray.forEach(cell => {
-    cell.addEventListener('click', onBoardClick, {once:true})
-})
+
 const winningCombinations = [
   [0, 1, 2],
   [3, 4, 5],
@@ -34,6 +27,8 @@ const winningCombinations = [
   [0, 4, 8],
   [2, 4, 6]
 ]
+let win 
+let current = currentPlayer ? 'X' : 'O'
 const title = document.getElementById('title');
 const p1 = document.getElementsByName('p1');
 const p2 = document.getElementsByName('p2');
@@ -46,8 +41,8 @@ title.addEventListener('click', function () {
     }
 });
 
-const winningMessageElement = document.getElementById('winningMessage')
-const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const winMessage = document.getElementById('winMessage')
+const winText = document.querySelector('[winText]')
 
 const restartbtn = document.getElementById('restartButton')
 restartbtn.addEventListener("click", buildInitialState)
@@ -55,7 +50,9 @@ restartbtn.addEventListener("click", buildInitialState)
 
 let fullBoard = gameState.board.flat(9)
 console.log(boardArray, board, gameState)
-    console.log(fullBoard)
+console.log(fullBoard)
+
+
 function startingTurn(){
             turn.count = Math.round(Math.random());
             if(turn.count === 1){
@@ -64,86 +61,74 @@ function startingTurn(){
             return
         }
 
-
-    function buildInitialState() {
-        fullBoard.forEach( (e) =>{
+function buildInitialState() {
+    fullBoard.forEach( (e) =>{
             e = null
         })
+    boardArray.forEach(cell => {
+    cell.removeEventListener('click',onBoardClick)
+    cell.addEventListener('click', onBoardClick, {once:true})
+})
         startingTurn()
+        renderState()
         
     
     
 
     }
-// buildInitialState()
-// render
-// function (arg){
-//     if(currentPlayer){
+    let player1 = 'x'
+    let player2 = 'o'
+function isDraw() {
+  return [...fullBoard].every(cell => {
+    return cell.contains('x') || cell.contains('o')
+  })
+}
 
-//     }
-//     arg.value = gameState.players[0]
-// }
-function checkWin() {
-    let winner = null;
-    winningCombos.forEach(function(combo, index) {
-        
-        ;
-};
 
+
+const turnmessage = document.getElementById('gameMessage')
 
 function renderState() {
     
     fullBoard.forEach((element, idx) => {
-
-        if(element){boardArray[idx].innerText = element 
-}})
+        
+        turnmessage.innerText= currentPlayer ? (`'It is ${player1}'s Turn!`) : `'It is ${player2}'s turn'`
+        if(element){
+            boardArray[idx].innerText = element }})
 }
 renderState()
 // maybe a dozen or so helper functions for tiny pieces of the interface
 
-// if(turn.count === (2 % 0 == 0)){
-// let player1 =  gameState.players[0]
-// }else{
-//     let player1 =  gameState.players[1]
-// }
 function addText(cell, currentPlayer){
     cell.innerText = currentPlayer ? 'x' : 'o'
 }
-function changeturn(){
+
+function winCheck(){
+    if (fullBoard.length = 9)
     
+}
+function changeturn(){
+        
         currentPlayer = !currentPlayer
+        // msg.innerText=(`It's ${current}'s turn`)
     }
 // listeners
 function onBoardClick() {
     console.log('clicked', event.target)
-    
     let cellIndex = boardArray.findIndex((cell)=>{
-
-   
     console.log(event.target)
     return cell === event.target
     }) 
+
+
     addText(event.target, currentPlayer)
     fullBoard[cellIndex] = currentPlayer ? 'x' : 'o'
-    checkWin()
+    winCheck()
     changeturn()
-    // currentPlayer ? `${players[0]}` : players[1]
-    
-    // players = players.reverse
-    // if(currentPlayer){
-    //     currentPlayer = false  
-    // }else{
-    //     currentPlayer = true
-    // }
-}
-function endGame(draw) {
-  if (draw) {
-    winMessage.innerText = 'Draw!'
-  } else {
-    winText.innerText = `${currentPlayer ? "X" : "O"} Wins!`
-  }
-}
+    renderState()
 
+   
+}
 
 
   // update state, maybe with another dozen or so helper functions...
@@ -153,5 +138,4 @@ function endGame(draw) {
 
 const gameboard = document.getElementById('board');
 // gameboard.addEventListener('click', onBoardClick); // 
-console.log(gameboard)
-console.log(...board, gameboard.children)
+buildInitialState()
